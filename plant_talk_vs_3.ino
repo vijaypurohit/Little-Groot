2a3bf6ae6cc20dc4cec759030b1319eed03f358c
Vijay Purohit <vijay.pu9@gmail.com>
2017-12-03 01:49:39 +0530

    midnight wars

--- plant_talk_vs_3.ino
+++ plant_talk_vs_3.ino
@@ -8,9 +8,8 @@
  * When the plant has been watered, the LED switches off and the string “Thank you, Vijay!” is sent once to your computer.
  * This version of program is for normal plant.
  * There are Three conditions acccording to plant water requirement.
- * 
- * Created on 17 April 2016 Under the guidance of Mr. ShivRam Meena
- * copyright 2016 Vijay Purohit <http://www.prepkida.com>
+ * There is also sonar module inculded
+ * Created on dec 03,2017 at Rajasthan Hackathon Udaipur
  * vijay.pu9@gmail.com
 */
 
@@ -18,12 +17,13 @@ int ledPin = 13;
 int buz = 12;
 int sensorPin = 0;
 int senval = 0;
+int flag ;   // old sensor value
 
 // sonar values
 int trigPin=9;
 int echoPin=10;
 int sonarled =8;
-long duration, distance;
+long duration, distance, oldDist;
 
 void setup() 
 {
@@ -35,56 +35,66 @@ void setup()
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(sonarled, OUTPUT);
-  
+}
+
+void sonar_read()
+{
+  //sonar distance led
+    digitalWrite(trigPin,HIGH);
+      delayMicroseconds(500);
+    digitalWrite(trigPin,LOW);
+      duration=pulseIn(echoPin,HIGH);
+      distance=(duration/2)/29.1;
+
+//    Serial.print("the thing is at ");
+//    Serial.print(distance);
+//    Serial.print("CM");
+//     Serial.println("");
+      
+    if(distance<=10)
+    {  
+      digitalWrite(sonarled,HIGH); 
+      digitalWrite(buz,HIGH);
+    }
+    else if(distance>=10)
+    {  
+      digitalWrite(sonarled,LOW); 
+      digitalWrite(buz,LOW); 
+    }
+    return;
 }
 
 void loop() 
 {
  senval = analogRead(sensorPin);;
-                                           // In case of no water 
- if(senval<400)
- {
-   digitalWrite(ledPin, HIGH);
-   digitalWrite(buz,HIGH);
-   Serial.print("I am dying, Please Help Me, Urgently Need Water\n"); 
-    delay(100);
-   digitalWrite(buz,LOW);
- }
+
+    sonar_read();
+    Serial.println(senval);
+     if(senval<400)                             // In case of no water 
+     {
+       digitalWrite(ledPin, HIGH);
+       Serial.println("I am dying, Please Help Me, Urgently Need Water\n"); 
+       digitalWrite(buz,HIGH);
+        delay(500);
+       digitalWrite(buz,LOW);
+     }
+                                              
+     if(senval<600 && senval>400)                 // In case of Little Water
+     {
            
-                                          // In case of Little Water
- if(senval<800 && senval>500)
- {
-   digitalWrite(buz, LOW);
-   digitalWrite(ledPin, HIGH); 
-   delay(500);
-   digitalWrite(ledPin, LOW); 
-   delay(500);
-   Serial.print("Feed Me, Feed Me, Need Water. I need more rooms\n");
- }
-
-                                         // In case of More Water
- if(senval>800) 
- {
-   digitalWrite(buz,LOW);
-   digitalWrite(ledPin, LOW);  
-   Serial.print("Thank You!,\nThank You very much, Vijay\n");
-   delay(1000);
- }
-
- //sonar distance led
-  digitalWrite(trigPin,HIGH);
-    delayMicroseconds(100);
-  digitalWrite(trigPin,LOW);
-    duration=pulseIn(echoPin,HIGH);
-    distance=(duration/2)/29.1;
-  Serial.print("the thing is at ");
-  Serial.print(distance);
-  Serial.println("CM");
-    delay(100);
-  
-  if(distance<=20)
-  {  digitalWrite(sonarled,HIGH); }
-  else if(distance>=20)
-  {  digitalWrite(sonarled,LOW);  }
+        digitalWrite(ledPin, HIGH); 
+       delay(500);
+       digitalWrite(ledPin, LOW); 
+       delay(500);
+       Serial.println("Feed Me, Feed Me, Need Water. I need more water\n");
+     }
+                                                  
+     if(senval>600)                             // In case of More Water
+     {
+       digitalWrite(buz,LOW);
+       digitalWrite(ledPin, LOW);  
+       Serial.println("Thank You!,\nThank You very much, Team Ravana\n");
+       delay(1000);
+     }
  
 }
